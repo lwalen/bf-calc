11658b0e78edc748d37ca873664b63f3840f8ff8
Lars Walen <lars@walen.me>
2014-08-13 20:10:14 -0700

    Moved tests into their own file

--- calculator.js
+++ calculator.js
@@ -82,43 +82,6 @@ function updateBoardFeet() {
 	}
 }
 
-function assert(f, a, e) {
-	console.log((f(a) == e) + "\t: " + f.name + "(" + a + ")" + " == " + e);
-}
-
-// CTRL + SHIFT + J. runTests()
-function runTests() {
-	parse_unit_tests = {'2"'   : 'inches',
-	                    "3'"   : 'feet',
-	                    "4''"  : 'inches',
-	                    "5.0'" : 'feet',
-	                    '6/7"' : 'inches'
-	                   }
-	$.each(parse_unit_tests, function(value, expected) {
-		assert(parseUnit, value, expected);
-	});
-
-	fraction_eval_tests = {'1/2'   : 0.5,
-	                       '2'     : 2,
-	                       '4.5/3' : 1.5
-	                      }
-
-	$.each(fraction_eval_tests, function(value, expected) {
-		assert(evaluateFractions, value, expected);
-	});
-
-	parse_input_tests = {'10'           : 10,
-	                     "5' 1''"       : 61,
-	                     "1/2'"         : 6,
-	                     "1.5'"         : 18,
-	                     "10.25' 3/4''" : 123.75
-	                    }
-
-	$.each(parse_input_tests, function(value, expected) {
-		assert(parseInput, value, expected);
-	});
-}
-
 $(function() {
    FastClick.attach(document.body);
 
@@ -138,7 +101,7 @@ $(function() {
 		e.preventDefault();
 	});
 
-	$('td').click(function() {
+	$('.keyboard td').click(function() {
 		td = $(this).text();
 
 		$('.length').val(function(i, v) {
