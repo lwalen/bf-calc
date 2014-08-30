ba38630e715f683756c6c71ad8b255544a8dab6a
Lars Walen <lars@walen.me>
2014-08-30 16:43:05 -0700

    Added tests for 1 1/2 type parsing

--- test.js
+++ test.js
@@ -4,18 +4,20 @@ function assert(f, a, e) {
 
 // CTRL + SHIFT + J. runTests()
 function runTests() {
-	parse_unit_tests = {'2"'   : 'inches',
-	                    "3'"   : 'feet',
-	                    "5.0'" : 'feet',
-	                    '6/7"' : 'inches'
+	parse_unit_tests = {'2"'     : 'inches',
+	                    "3'"     : 'feet',
+	                    "5.0'"   : 'feet',
+	                    '6/7"'   : 'inches',
+	                    "1 3/4'" : 'feet'
 	                   }
 	$.each(parse_unit_tests, function(value, expected) {
 		assert(parseUnit, value, expected);
 	});
 
-	fraction_eval_tests = {'1/2'   : 0.5,
-	                       '2'     : 2,
-	                       '4.5/3' : 1.5
+	fraction_eval_tests = {'1/2'    : 0.5,
+	                       '2'      : 2,
+	                       '4.5/3'  : 1.5,
+	                       '10 1/2' : 10.5
 	                      }
 
 	$.each(fraction_eval_tests, function(value, expected) {
@@ -24,9 +26,10 @@ function runTests() {
 
 	parse_input_tests = {'10'           : 10,
 	                     "5' 1\""       : 61,
-	                     "2'5\""       : 29,
+	                     "2'5\""        : 29,
 	                     "1/2'"         : 6,
 	                     "1.5'"         : 18,
+	                     "3 1/2'"       : 42,
 	                     "10.25' 3/4\"" : 123.75
 	                    }
 
